9d27a944ad11be0c35d4702cdf269ad663c2f08d
Nicholas Capo <nicholas@vubeology.com>
2014-02-18 12:03:19 -0600

    Fix update command
    * Update was previously checking out the old version, then
    * looking for the lastest commit, when it should checkout the specified
    * branch and then look for the latest commit

--- update/update.go
+++ update/update.go
@@ -19,16 +19,24 @@ func Update(deps dep.DependencyMap, name string, branch string) {
 		util.Fatal(colors.Red("Dependency Name '" + name + "' not found in deps.json"))
 	}
 
+	// record the old version
 	oldVersion := d.Version
+
+	// temporarily use the branch
+	d.Version = branch
 
 	pwd := util.Pwd()
 	util.Cd(d.Path())
 	d.VCS.Checkout(d)
 	d.VCS.Update(d)
+
+	// get the last commit on the newly checked out branch
 	v, err := d.VCS.LastCommit(d, branch)
 	if err != nil {
 		util.Fatal(err)
 	}
+
+	// set the version to be the last commit
 	d.Version = v
 
 	util.PrintIndent(colors.Blue(name) + " (" + oldVersion + " --> " + d.Version + ")")
