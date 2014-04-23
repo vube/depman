42f765886fdc12c826af85743fbb0eea6550d364
Nicholas Capo <nicholas@vubeology.com>
2014-04-23 16:10:39 -0500

    add: Cleanup and bug fix
    * Remove ability to add a second dep since that never worked anyway
    * Remove promptBool (newly unused)
    * Update Documentation

--- add/add.go
+++ add/add.go
@@ -1,7 +1,7 @@
-// Package add implements functions for adding a dependency
+// Package add implements functions for interactively adding a dependency
 package add
 
-// Copyright 2013 Vubeology, Inc.
+// Copyright 2014 Vubeology, Inc.
 
 import (
 	"fmt"
@@ -13,9 +13,8 @@ import (
 	"github.com/vube/depman/util"
 )
 
-// Add interactively prompts the user for details of a dependency, adds it to deps.json, and writes out the file
+// Add interactively prompts the user for details of a dependency, adds it to deps.json, writes out the file, and installs the dependencies
 func Add(deps dep.DependencyMap, name string) {
-	var cont = true
 	_, exists := deps.Map[name]
 	if exists {
 		util.Fatal(colors.Red("Dependency '" + name + "'' is already defined, pick another name."))
@@ -23,28 +22,28 @@ func Add(deps dep.DependencyMap, name string) {
 
 	util.Print(colors.Blue("Adding: ") + name)
 
-	for cont {
-		d := new(dep.Dependency)
-		d.Type = promptType("Type", "git, git-clone, hg, bzr")
-		if d.Type == dep.TypeGitClone {
-			d.Repo = promptString("Repo", "git url")
-		} else {
-			d.Repo = promptString("Repo", "go import")
-		}
-		d.Version = promptString("Version", "hash, branch, or tag")
-		if d.Type == dep.TypeGitClone {
-			d.Alias = promptString("Alias", "where to install the repo")
-		}
-		deps.Map[name] = d
-		cont = promptBool("Add another", "y/N")
+	d := new(dep.Dependency)
+	d.Type = promptType("Type", "git, git-clone, hg, bzr")
+
+	if d.Type == dep.TypeGitClone {
+		d.Repo = promptString("Repo", "git url")
+	} else {
+		d.Repo = promptString("Repo", "go import")
 	}
+
+	d.Version = promptString("Version", "hash, branch, or tag")
+
+	if d.Type == dep.TypeGitClone {
+		d.Alias = promptString("Alias", "where to install the repo")
+	}
+
+	deps.Map[name] = d
 
 	for name, d := range deps.Map {
 		err := d.SetupVCS(name)
 		if err != nil {
 			delete(deps.Map, name)
 		}
-
 	}
 
 	err := deps.Write()
@@ -57,28 +56,14 @@ func Add(deps dep.DependencyMap, name string) {
 	return
 }
 
-// Promt the user with question and return a string answer
+// promptString prompts the user with question and returns a string answer
 func promptString(question string, details string) (answer string) {
 	fmt.Print(colors.Blue(question) + " (" + details + "): ")
 	fmt.Scanln(&answer)
 	return
 }
 
-// Prompt the user with question and return a bool answer
-func promptBool(question string, details string) (answer bool) {
-	str := promptString(question, details)
-
-	switch str {
-	case "y", "yes":
-		answer = true
-	default:
-		answer = false
-	}
-
-	return
-}
-
-// Prompt the user with question check that the answer is a valid dep type and then return it
+// promptType prompts the user with question, checks that the answer is a valid dep type and then return it
 func promptType(question string, details string) (t string) {
 	for {
 		t = promptString(question, details)
